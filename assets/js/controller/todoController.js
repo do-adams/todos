018e264bdda0371557fb34b8672301ba68791cae
Damián Adams <contact@mianlabs.com>
2018-03-09 19:19:37 -0400

    Implement the text edit functionality for existing todo items

--- assets/js/controller/todoController.js
+++ assets/js/controller/todoController.js
@@ -49,6 +49,52 @@ class TodoController {
 			'keypress', this._updateViewAfterEvent(this._newTodoHandler));
 		this._view.$todoList.addEventListener(
 			'click', this._updateViewAfterEvent(this._todoEventHandler));
+		
+		this._view.$todoList.addEventListener(
+			'dblclick', (e) => {
+				const $target = e.target;
+				const tagName = $target.tagName;
+				const $todo = $target.parentNode;
+
+				if (tagName.toLowerCase() === 'label' && 
+				!this._isTodoElemInEditMode($todo)) {
+					this._toggleEditMode($todo, true);
+				}
+			});
+
+		this._view.$todoList.addEventListener(
+			'focusout', this._updateViewAfterEvent((e) => {
+				const $target = e.target;
+				const tagName = $target.tagName;
+				const $todo = $target.parentNode;
+
+				if (tagName.toLowerCase() === 'label') {
+					this._toggleEditMode($todo, false);
+					if ($target.innerText.trim() === '') {
+						this._removeTodoElem($todo);
+					} else {
+						this._saveTodoElemChanges($todo);
+					}
+				}
+			}));
+
+		this._view.$todoList.addEventListener(
+			'keypress', this._updateViewAfterEvent((e) => {
+				const $target = e.target;
+				const tagName = $target.tagName;
+				const $todo = $target.parentNode;
+
+				if (tagName.toLowerCase() === 'label' && 
+				e.keyCode === this._ENTER_KEY) {
+					this._toggleEditMode($todo, false);
+					if ($target.innerText.trim() === '') {
+						this._removeTodoElem($todo);
+					} else {
+						this._saveTodoElemChanges($todo);
+					}
+				}
+			}));
+
 		this._view.todoFooter.$allFilter.addEventListener(
 			'click', this._todosDisplayFilterHandler.bind(this));
 		this._view.todoFooter.$activeFilter.addEventListener(
@@ -115,6 +161,8 @@ _toggleCheckboxElem($todo, checked) {
 
 	/**
 	 * Builds a generic unchecked and unindexed todo element.
+	 * A todo element consists of a checkbox icon, a text label, 
+	 * and a delete button.
 	 */
 	_buildTodoElem(text) {
 		const $todo = document.createElement('li');
@@ -266,6 +314,68 @@ _toggleCheckboxElem($todo, checked) {
 		return completed;
 	}
 
+	_isTodoElemInEditMode($todo) {
+		const $label = $todo.querySelector('label');
+		const contentEditableValue = $label.getAttribute('contenteditable');
+		return (contentEditableValue === null || contentEditableValue === '' 
+			|| contentEditableValue === 'false')  ? false : true;
+	}
+
+	_toggleEditMode($todo, edit) {
+		if (edit === undefined) {
+			edit = $todo.classList.contains('edit-mode');
+		} 
+
+		// Nifty solution for setting the cursor to the end of the line
+		// as discussed in: https://stackoverflow.com/a/3866442
+		function setEndOfContentEditable(contentEditableElement) {
+			let range, selection;
+			if(document.createRange)// Firefox, Chrome, Opera, Safari, IE 9+
+			{
+				range = document.createRange(); // Create a range (a range is a like the selection but invisible)
+				range.selectNodeContents(contentEditableElement); // Select the entire contents of the element with the range
+				range.collapse(false); // Collapse the range to the end point. false means collapse to end rather than the start
+				selection = window.getSelection(); // Get the selection object (allows you to change selection)
+				selection.removeAllRanges(); // Remove any selections already made
+				selection.addRange(range); // Make the range you have just created the visible selection
+			}
+			else if(document.selection)// IE 8 and lower
+			{ 
+				range = document.body.createTextRange(); // Create a range (a range is a like the selection but invisible)
+				range.moveToElementText(contentEditableElement); // Select the entire contents of the element with the range
+				range.collapse(false); // Collapse the range to the end point; false means collapse to end rather than the start
+				range.select(); // Select the range (make it the visible selection)
+			}
+		}
+
+		const $checkBox = $todo.querySelector('i');
+		const $label = $todo.querySelector('label');
+
+		if (edit) {
+			$checkBox.classList.add('hidden');
+			
+			$todo.classList.add('edit-mode');
+			$label.setAttribute('contenteditable', 'true');
+			$label.focus();
+			setEndOfContentEditable($label);
+		} else {
+			$checkBox.classList.remove('hidden');
+
+			$todo.classList.remove('edit-mode');
+			$label.setAttribute('contenteditable', 'false');
+		}
+
+		return edit;
+	}
+
+	_saveTodoElemChanges($todo) {
+		const key = $todo.dataset.key;
+		const model = this._store.getTodo(key);
+		model.text = $todo.querySelector('label').innerText.trim();
+		model.isCompleted = $todo.classList.contains('completed');
+		this._store.setTodo(key, model);
+	}
+
 	/**
 	 * Removes an indexed todo element from the view and the store.
 	 */
@@ -273,8 +383,10 @@ _toggleCheckboxElem($todo, checked) {
 		const key = $todo.dataset.key;
 		if (key == undefined) {
 			throw new Error('data-key property not found on todo element');
+		} 
+		if ($todo.parentNode === this._view.$todoList) {
+			this._view.$todoList.removeChild($todo);
 		}
-		this._view.$todoList.removeChild($todo);
 		this._store.removeTodo(key);
 	}
 
